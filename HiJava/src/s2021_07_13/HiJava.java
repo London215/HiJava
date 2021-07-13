f17451935b3bcc9e5d0f906e0ee1ecbdef7e1185
jibeo <jibeo@DESKTOP-542NRJI>
2021-07-13 10:42:48 +0900

    두번쨰 소스

--- HiJava/src/s2021_07_13/HiJava.java
+++ HiJava/src/s2021_07_13/HiJava.java
@@ -4,7 +4,8 @@ public class HiJava {
 
 	public static void main(String[] args) {
 		System.out.println("¾È³ç ±êÇãºê");
-		System.out.println();
+		
+		System.out.println("¶Ç ¸¸³ª³× ±êÇãºê");
 	}
 
 }
